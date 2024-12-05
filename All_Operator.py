392a95783184ae4271a40aaacf046794652c47f4
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-05 10:55:37 +0530

    Add files via upload

---
+++ All_Operator.py
@@ -0,0 +1,13 @@
+a, b = 5, 3
+print(a+b, a-b, a*b, a/b, a%b, a**b, a//b)
+
+# This showcases:
+#
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponentiation (**)
+# Floor Division (//)
+# All in a single line.
